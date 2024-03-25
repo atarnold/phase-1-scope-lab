283b8432af836a8b90bf72923d9045af6e6bd189
arnold <arnoldkmaina@gmail.com>
2024-03-25 16:04:45 +0300

    Scope

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+    customerName = customerName.toLocaleUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'not my favorite';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'my favorite'
+}
+
